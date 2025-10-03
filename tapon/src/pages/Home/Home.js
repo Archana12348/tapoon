c498bb9293510e732ac52b5f79a0394acbd47961
Archana Gupta <archnagupta8656@gmail.com>
2025-10-03 16:20:12 +0530

    homepage

---
+++ tapon/src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import Slider from "../../components/common/slider/Slider";
+
+export default function Home() {
+  return (
+    <>
+      <Slider />
+      {/* Add more sections here */}
+    </>
+  );
+}
