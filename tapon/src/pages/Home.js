633f462badd9d0b6817a23420ccdb4be9d0e1191
pwtmuskansharma <pwtmuskansharma@gmail.com>
2025-10-03 16:14:56 +0530

    Testtimonials , CTA and accurate is done only styling

--- tapon/src/pages/Home.js
+++ tapon/src/pages/Home.js
@@ -1,11 +1,26 @@
 // import Slider from "../components/Slider";
+import { HowItWorksSection } from "../components/sections/howitworks";
+import TestimonialsSection from "../components/sections/testimonialsSection";
+import { CTASection } from "../components/sections/cta";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <TestimonialsSection
+        label="Customer Stories"
+        title="What Our Clients Say"
+        subtitle="Join thousands of professionals who have transformed their
+            networking with our NFC cards."
+      />
+      <h1>Hello</h1>
+      <HowItWorksSection
+        label="Simple Process"
+        title="How It Works"
+        subtitle="Get started with your NFC card in four simple steps and
+            revolutionize the way you network."
+      />
+      <CTASection />
+
       {/* Add more sections here */}
     </>
   );
